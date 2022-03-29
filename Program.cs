780241119eecce041c3661a68efbffbc1a6cca95
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2022-03-29 18:37:34 +0000

    Removed Blazorise package.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,3 @@
-using Blazorise;
-using Blazorise.Bulma;
-using Blazorise.Icons.FontAwesome;
-
 using Cv;
 
 using Microsoft.AspNetCore.Components.Web;
@@ -11,7 +7,5 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddBlazorise().AddBulmaProviders().AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
