f0cbad2951f4e1ef55d2cfd31f382ebc51a4a0d4
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2022-03-29 16:17:44 +0000

    Removed css and template stuff, and added Blazorise.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
+using Blazorise;
+using Blazorise.Bulma;
+using Blazorise.Icons.FontAwesome;
+
+using Cv;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using cv;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorise().AddBulmaProviders().AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
